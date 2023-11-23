5a1f71777bfaf3967827c21fca12cf9673780ee2
acse-5ba1b13c <xc1923@ic.ac.uk>
2023-11-23 14:47:00 +0000

    add all tests

--- tests/test_solver1.py
+++ tests/test_solver1.py
@@ -156,6 +156,62 @@ def test_scenario(planet):
 
 
 
+# Define the Planet instance with specified parameters
+simple_planet = Planet(
+    Cd=1.0,
+    Ch=0,
+    Q=np.inf,
+    Cl=0,
+    alpha=0,
+    Rp=np.inf,
+    g=0,
+    H=8000.0,
+    rho0=1.2,
+)
+
+# Define an analytical equation for comparison in tests
+def analytical_equation(altitude):
+    Cd = 1.0
+    Ch = 0
+    Cl = 0
+    v0 = 19000.0
+    z0 = 100000.0  # Initial height in meters
+    m = 5.387831e08  # Mass in kg
+    theta = np.radians(45)  # Convert angle to radians
+    H = 8000.0  # Atmospheric scale height in meters
+    r = 35.0
+    g = 0
+    rho0 = 1.2
+
+    K = (Cd * rho0 * np.pi * r**2) * H / (2 * m * np.sin(theta))
+    B = v0 / (np.exp(np.exp(-z0 / H) * K))
+
+    return B * np.exp(np.exp(-altitude / H) * K)
+
+# Decorate this function with @pytest.fixture to make it a fixture
+@pytest.fixture
+def approx_results_fixture():
+    inputs = {
+        "radius": 35.0,
+        "angle": 45.0,
+        "strength": 1e7,
+        "density": 3000.0,
+        "velocity": 19e3,
+    }
+
+    return simple_planet.solve_atmospheric_entry(**inputs)
+
+# Test function using the fixture
+def test_no_gravity_flat_scenario(approx_results_fixture):
+    for index, row in approx_results_fixture.iterrows():
+        calculated_velocity = row["velocity"]
+        approx_velocity = analytical_equation(row["altitude"])
+        assert np.isclose(
+            calculated_velocity, approx_velocity, rtol=0.1
+        ), f"Row {index}: Calculated Velocity is not close to Approx Velocity"
+
+
+
 
 @pytest.fixture
 def earth():
